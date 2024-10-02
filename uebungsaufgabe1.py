a29aabd33f550da04c5f35f21495f04c40f2d025
nicole <nicole07041994@gmail.com>
2024-10-02 23:12:46 +0200

    add uebungsaufgabe1.py

---
+++ uebungsaufgabe1.py
@@ -0,0 +1,9 @@
+# Programm, das prüft, ob eine Zahl positiv, negativ oder null ist
+zahl = int(input("Bitte geben Sie eine Zahl ein: "))
+
+if zahl > 0:
+    print("Die Zahl ist positiv.")
+elif zahl < 0:
+    print("Die Zahl ist negativ.")
+else:
+    print("Die Zahl ist null.")
