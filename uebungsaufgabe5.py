380144e86e8622df27fca9768f75f9f7d9450021
nicole <nicole07041994@gmail.com>
2024-10-02 23:19:26 +0200

    add uebungsaufgabe5.py

---
+++ uebungsaufgabe5.py
@@ -0,0 +1,10 @@
+# Programm, das die Zahlen von 1 bis 100 durchläuft und Fizz, Buzz oder FizzBuzz ausgibt
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
