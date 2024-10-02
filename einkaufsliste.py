2f5ae89ba23f0b572bd00651acc785135c9a2a7f
nicole <nicole07041994@gmail.com>
2024-10-02 22:00:59 +0200

    add einkaufsliste.py

---
+++ einkaufsliste.py
@@ -0,0 +1,35 @@
+shoppinglist = [Einkaufsliste]
+def add_item():
+    item = input("Bitte gib den Artikel ein, der zur Einkaufsliste hinzugefügt werden soll: ")
+    shoppinglist.append(item)
+    print(f"{item} wurde der Einkaufsliste hinzugefügt.")
+
+    def show_shoppinglist():
+    if shoppinglist:
+        print("Dece == "3":
+            print("Programm wird beendet! Auf Wiedersehen")
+            break
+        else:
+            print("Ungültige Auswahl. Bitte wähle 1, 2 oder 3.")
+
+          
+          if __name__ == "__main__":
+    main()ine Einkaufsliste:")
+        for item in shoppinglist:
+            print(f"- {item}")
+    else:
+        print("Deine Einkaufsliste ist leer.")
+
+        def main():
+    while True:
+        print("\n----- Einkaufsliste -----")
+        print("1. Artikel zur Einkaufsliste hinzufügen")
+        print("2. Einkaufsliste anzeigen")
+        print("3. Programm beenden")
+        choice = input("Bitte wähle eine Option (1, 2 oder 3): ")
+
+        if choice == "1":
+            add_item()
+        elif choice == "2":
+            show_shoppinglist()
+        elif choi
